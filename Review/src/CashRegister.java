98236dbd49bfea62a36d6c0ec3a0bac48af4ea51
s1249736 <s1249736@HH306-25WL55890.monmouth.edu>
2020-01-29 20:07:58 -0500

    This is the Review Lab

---
+++ Review/src/CashRegister.java
@@ -0,0 +1,18 @@
+
+public class CashRegister {
+
+	private double purchase;
+	private double payment;
+	
+	public CashRegister(double purchase, double payment) {
+		purchase = purchase;
+		payment = payment;
+	}
+	
+	public CashRegister() {
+		purchase = 0;
+		payment = 0 ;
+	}
+	
+	
+}
